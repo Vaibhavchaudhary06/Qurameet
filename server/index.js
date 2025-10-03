288270bf6bc1e8f6e44687385fc2626657b1103b
Vaibhav Chaudhary <vaibhavchaudhary8181@gmail.com>
2025-10-04 02:55:10 +0530

    temp: server update before switching

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const { Server } = require("socket.io");
 
 const app = express();
 app.use(cors());
-app.get("/", (_req, res) => res.send("Qura Meet Signaling Server running ✅"));
+app.get("/", (_req, res) => res.send("Qura Meet Signaling Server running "));
 
 const server = http.createServer(app);
 
